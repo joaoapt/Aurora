99904cf1d60742ad02b344480fe6ca59f2b521ca
joaoapt <104604232+joaoapt@users.noreply.github.com>
2022-10-10 09:54:03 -0300

    gfg

--- BD/ddl.sql
+++ BD/ddl.sql
@@ -1,11 +1,114 @@
-create database aurorabsDB;
+create database aurorabksDB;
 
-drop database aurorabsDB;
+-- drop database aurorabksdb;
 
-use aurorabsDB;
+use aurorabksDB;
 
-create table tb_usuario (
-		id_usuario 		int primary key auto_increment,
-        ds_email		varchar(200),
+create table tb_admin (
+		id_admin 		int primary key auto_increment,
+        ds_email		varchar(100),
         ds_senha		varchar(20)
-)
+);
+
+create table tb_categoria (
+    id_categoria int primary key auto_increment,
+    ds_categoria varchar(50)
+);
+
+create table tb_produto (
+	id_produto 			int primary key auto_increment,
+	id_categoria		int,
+    nm_livro			varchar(150),
+    nm_autor			varchar(100),
+    nm_editora			varchar(100),
+    nm_idioma			varchar(60),
+    nr_isbn13			int default null,
+    nr_isbn10			int default null,
+    vl_preco			decimal(15),
+    nm_original			varchar(150),
+    ds_sinopse			varchar(450),
+    ds_versao			varchar(10),
+    nr_pagina			int,
+    nr_volume			int,
+    nr_largura			int,
+    nr_comprimento		int,
+    ds_imagem           varchar(300),
+    foreign key (id_categoria) references tb_categoria(id_categoria)
+);
+﻿
+create table tb_usuario(
+    id_usuario_conta int primary key auto_increment,
+    ds_cpf varchar(14),
+    nm_usuario varchar(200)
+);
+﻿
+create table tb_usuarioendereco(
+    id_usuario_endereco int primary key auto_increment,
+    id_usuario_conta int,
+    ds_endereco varchar(200),
+    nr_endereco int,
+    nr_cep varchar(9),
+    ds_referencia varchar(350),
+    ds_cidade varchar(100),
+    ds_bairro varchar(100),
+    ds_estado varchar(80),
+    ds_pais varchar(90),
+    ds_complemento varchar(30),
+    foreign key (id_usuario_conta) references tb_usuario(id_usuario_conta)
+);
+
+create table tb_usuariofavorito(
+	id_usuario_favorito int primary key auto_increment,
+    id_usuario_conta int,
+    id_produto int,
+    foreign key (id_usuario_conta) references tb_usuarioconta(id_usuario_conta),
+    foreign key (id_produto) references tb_produto(id_produto)
+);
+
+
+create table tb_usuariologin(
+	id_usuario_login int primary key auto_increment,
+    id_usuario_conta int,
+    ds_email varchar(100),
+    ds_senha varchar(100),
+    foreign key (id_usuario_conta) references tb_usuario(id_usuario_conta)
+);
+
+
+create table tb_pedido(
+    id_pedido int primary key auto_increment,
+    id_usuario_conta int,
+    id_usuario_endereco int,
+    tp_forma_pagamento int,
+    ds_status varchar(150),
+    dt_pedido date,
+    foreign key (id_usuario_endereco) references tb_usuarioendereco(id_usuario_endereco),
+    foreign key (id_usuario_conta) references tb_usuario(id_usuario_conta)
+);
+
+
+create table tb_pedidoitem(
+    id_pedido_item int primary key auto_increment,
+    id_pedido int,
+    id_produto int,
+    foreign key (id_pedido) references tb_pedido(id_pedido),
+    foreign key (id_produto) references tb_produto(id_produto)
+);﻿
+
+create table tb_pagamentocartao(
+    id_pagamentocartao int primary key auto_increment,
+    id_pedido int,
+    nm_pessoa varchar(200),
+    nr_cartao int,
+    cod_seguranca int(3),
+    dt_validade varchar(5),
+    ds_cpf varchar(14),
+    foreign key (id_pedido) references tb_pedido(id_pedido)
+);
+﻿
+create table tb_pagamento_boleto(
+    id_pagamento_boleto int primary key auto_increment,
+    id_pedido int,
+    cod_boleto int,
+    foreign key (id_pedido) references tb_pedido(id_pedido)
+);
--- BD/dml.sql
+++ BD/dml.sql
@@ -1,14 +1,56 @@
-use aurorabsDB;
+use aurorabksDB;
 
 -- criar login
-INSERT INTO tb_usuario (ds_email, ds_senha)
-     VALUES ('joao@email.com.br', '1234');
+INSERT INTO tb_usuariologin (ds_email, ds_senha)
+     VALUES ('j', '1234');
 
 -- login
-select id_usuario 		id,
-       ds_email			email
-  from tb_usuario
- where ds_email 		= 'joao@email.com.br'
-   and ds_senha			= '1234';
+select id_admin 		 id
+  from tb_admin
+ where ds_email 		 = 'joao@email.com.br'
+   and ds_senha			 = '1234';
    
-select * from tb_usuario;
+   -- login
+select id_usuario 		 id
+  from tb_usuariologin
+ where ds_email 		 = 'joao@email.com.br'
+   and ds_senha			 = '1234';
+   
+-- usuario referente a conta
+select nm_usuario     nome,
+	   ds_cpf		        cpf
+  from tb_usuario    
+ where id_usuario    = 12 ;
+
+-- endereço do usuario
+select id_usuario_endereco      id,
+       ds_endereco              endereco,
+       nr_endereco              numero,
+       nr_cep                   cep,
+       ds_referencia            referencia,
+       ds_cidade                cidade,
+       ds_bairro                bairro,
+       ds_estado                estado,
+       ds_pais                  pais,
+       ds_complemento           complemento
+  from tb_usuarioendereco
+ where  id_usuario         = 12;
+
+-- inserir dados
+insert into tb_produto (nm_categoria, nm_livro, nm_autor, nm_editora, nr_isbn13, nr_isbn10, vl_preco, nm_original, ds_sinopse, ds_versao, nr_pagina, nr_volume, nr_largura, nr_comprimento)
+	values('filosofia','ameno','alan','mono',320,402,19,'amenonolo','martelo de ferro','2',23,42,24,43);
+   
+-- inserir imagem
+insert into tb_produto (ds_imagem)
+	values('https://tocalivros.s3.amazonaws.com/images/audiolivros/200/b/o/box-as-grandes-historias-de-sherlock-holmes-arthur-conan-doyle-1051105.jpg');
+
+-- editar produto
+update tb_produto
+set nm_livro = 'oi',  nm_autor = 'oi', nm_editora = 'oi', nr_isbn13 = 120890887969856, nr_isbn10 = null, vl_preco = 150.89, nm_original = 'hi' 
+where id_produto = 1; 
+
+-- consultar produtos
+select * from tb_produto;
+
+-- consulta nome categoria
+select * from tb_categoria;
