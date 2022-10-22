9bbd0ed619f8e9e848538b6978e08eacedd63874
nick0205-br <nicolasdlopes@gmail.com>
2022-10-22 16:41:04 -0300

    editar ok

--- site/src/api/admin/cadastrar-livro.js
+++ site/src/api/admin/cadastrar-livro.js
@@ -24,8 +24,23 @@ export async function cadastrar(categoria, livro, autor, editora, idioma, isbn13
     return resposta.data;
 }
 
-export async function alterarProduto(id, nome, preco, destaque, idDepartamento, categorias) {
-    await api.put('/admin/livro/' + id, { nome, preco, destaque, idDepartamento, categorias });
+export async function alterarProduto(id, categoria, livro, autor, editora, idioma, isbn13, isbn10, preco, original, sinopse, versao, pagina, volume, largura, comprimento) {
+    await api.put('/admin/livro/' + id, {         
+        categoria:categoria,
+        livro:livro,
+        autor:autor,
+        editora:editora,
+        idioma:idioma,
+        isbn13:isbn13,
+        isbn10:isbn10,
+        preco:preco,
+        original:original,
+        sinopse:sinopse,
+        versao:versao,
+        pagina:pagina,
+        volume:volume,
+        largura:largura,
+        comprimento:comprimento });
 }
 
 
@@ -63,16 +78,3 @@ export async function buscarProdutoPorId(id) {
     const r = await api.get('/admin/livro/' + id);
     return r.data;
 }
-
-////imagem capa do livro
-//export async function enviarImagem(id, imagem) {
-//    const formData = new FormData();
-//    formData.append('capa-livro', imagem);
-//
-//      const resposta = await api.put(`/img/${id}/capa/livro`, formData, {
-//        headers: {
-//            "Content-Type": "multipart/form-data"
-//        };
-//    });
-//    return resposta.status;
-//}
