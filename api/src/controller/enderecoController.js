930baac50ac2dca7ac5ccfb61d3a73283eeca850
nick0205-br <nicolasdlopes@gmail.com>
2022-10-23 08:55:01 -0300

    Updates

--- api/src/controller/enderecoController.js
+++ api/src/controller/enderecoController.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { listar, salvar, alterarEndereco } from "../repository/EnderecoRepository.js";
+import { listar, salvar, alterarEndereco } from "../repository/enderecoRepository.js";
 const server = Router();
 
 
