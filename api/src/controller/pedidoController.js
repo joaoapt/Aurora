9061d47b4cff0e3c25d13f4c7dcfb0dfa5268098
joaoapt <104604232+joaoapt@users.noreply.github.com>
2022-11-07 11:51:51 -0300

    cu pra carralho

--- api/src/controller/pedidoController.js
+++ api/src/controller/pedidoController.js
@@ -1,10 +1,8 @@
 
 import { Router } from "express";
-import { consultarStatus } from "../repository/pedidoRepository.js";
-import { consultarPedidoUsuario } from "../repository/pedidoRepository.js";
-import { inserirPagamentoBoleto, inserirPagamentoCartao, inserirPedido, inserirPedidoItem, alterarStatus} from "../repository/pedidoRepository.js";
+import { inserirPagamentoBoleto, inserirPagamentoCartao, inserirPedido, inserirPedidoItem } from "../repository/pedidoRepository.js";
 import { buscarProdutoPorId } from "../repository/produtoRepository.js";
-import { criarNovoPedidoBoleto, criarNovoPedidoCartao } from "../service/novoPedido.js";
+import { criarNovoPedidoBoleto, criarNovoPedidoCartao } from "../service/index.js";
 import { criarBoleto } from "../service/novopedido.js";
 const server = Router();
 
@@ -36,65 +34,8 @@ server.post('/api/pedido/cartao/:idUsuario/', async (req, resp) => {
     }
 })
 
-//alterar status
-server.put('/admin/pedido/:id', async (req, resp) => {
-    try {
-        const id = req.params.id;
-        const pedido = req.body;
-        await alterarStatus(id, pedido);
-        
-        resp.status(204).send();
 
-    }
-    catch (err) {
-        resp.status(400).send({
-            erro: err.message
-        })
-    }
-})
-
-//consultar todos status e numero pedido tela admin
-server.get('/consultar/status', async (req,resp) => {
-    try {
-        const id = req.params.id;
-        const resposta = await consultarStatus(id);
-        resp.send(resposta)
-    } catch (err) {
-        resp.status(401).send({
-            erro: err.message
-        });
-    }
-})
-
-//consultar pedido especifico
-server.get('/usuario/consultar/pedido', async (req,resp) => {
-    try {
-        const idPedido = req.params.idPedido;
-        const idUsuario = req.params.idUsuario;
-        const resposta = await consultarPedidoUsuario(idPedido, idUsuario);
-        resp.send(resposta)
-    } catch (err) {
-        resp.status(401).send({
-            erro: err.message
-        });
-    }
-})
-
-//Numero pedido e status tela usuario
-server.get('/consultar/pedido', async (req,resp) => {
-    try {
-        const id = req.params.id;
-        const resposta = await consultarPedido(id);
-        resp.send(resposta)
-    } catch (err) {
-        resp.status(401).send({
-            erro: err.message
-        });
-    }
-})
-
-
-server.post('/api/pedido/boleto/:idUsuario/', async (req, resp) => {
+server.post('/api/pedido/cartao/:idUsuario/', async (req, resp) => {
    try {
        const { idUsuario } = req.params;
        const info = req.body;
